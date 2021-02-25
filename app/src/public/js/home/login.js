6009f9852be490580a29bef9c4231b418b54270a
1999onell <1999onell@gmail.com>
2021-02-26 08:20:25 +0900

    16 fetch를 이용해서 로그인 요청

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,7 +10,20 @@ function login() {
     const req = {
         id: id.value,
         psword: psword.value,
-
     };
-    console.log(req);
+    //fetch: 백엔드로부터 데이터를 받아오기위한 Web API 이다.
+    //브라우저에 내장된 기능이다.
+    //fetch()함수는 첫번째 인자로 URL, 두 번째 인자로 옵션 객체를 받고
+    //Promise 타입의 객체를 반환한다.
+    //API호출이 성공했을 경우에는 응답(response)객체를 resolve하고,
+    //실패있을 경우에는 예외(error)객체를 reject한다.
+    //fetch로 프론트단의 데이터를 서버로 보내보자.
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req),
+    });
 }
