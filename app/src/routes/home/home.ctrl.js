efa7f1e9fd66febbd348d87ae1f9c21778bcf51f
1999onell <1999onell@gmail.com>
2021-02-26 08:31:04 +0900

    17 로그인 API 만들기 in 서버

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,16 +1,24 @@
 "use strict";
 //라우팅되서 온걸 무언가 실행시켜줌.
 
-const home = (req, res) => {
-    res.render("home/index");
-};
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
 
-const login = (req, res) => {
-    res.render("home/login");
-};
-
+const process = {
+    login: (req, res) => {
+    //req.body를 보려면 app.js에 body-parse 모듈을 설치해야함.
+        console.log(req.body);
+    }
+}
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
