7ed4e8b99f8abd3f5a4eb726cdb649221efdb031
1999onell <1999onell@gmail.com>
2021-02-26 21:34:31 +0900

    25 회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,15 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if (!id.value) return alert("아이디를 입력해 주십시오.");
+    if (psword !== confirmPsword) return alert("비밀번호가 일치하지 않습니다.");
+    
+
+    
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };
 
 
