b4688fe5bd46d4c492360b57155ad9f9b7bd82ca
1999onell <1999onell@gmail.com>
2021-02-26 09:47:36 +0900

    19 프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -28,5 +28,14 @@ function login() {
         //서버로부터 response를 받으려면 then을 사용한다.
         //그런데 promise타입의 데이터가 온다. then을 한번 더 사용하자.
     }).then((res) => res.json())
-      .then(console.log);
+      .then((res) => {
+          if (res.success) {
+              location.href="/";
+          } else {
+              alert(res.msg);
+          }
+      })
+      .catch((err) => {
+          console.error(new Error("로그인 중 에러 발생"));
+      });
 }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,7 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
-router.post("/login", ctrl.process.login);
+// router.post("/login", ctrl.process.login);
 
   
 
