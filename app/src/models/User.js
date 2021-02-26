f486eb71b67f9e729982d31ab5b9c6e51fa67e3d
1999onell <1999onell@gmail.com>
2021-02-27 08:44:54 +0900

    27 fs모듈을 통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,18 +7,19 @@ class User {
         this.body = body;
     }
 
-    login() {
+    async login() {
         const client = this.body;
-        UserStorage.getUserInfo(client.id);
+        //await은 프로미스를 반환하는 애에게 주는 옵션임
+        const { id, psword } = await UserStorage.getUserInfo(client.id);
     
-        // if(id) {
-        //     if (id === client.id && psword === client.psword) {
-        //         return { success: true };
-        //     }
-        //     return { success: false, msg: "비밀번호가 틀렸습니다."};
-        // }
+        if(id) {
+            if (id === client.id && psword === client.psword) {
+                return { success: true };
+            }
+            return { success: false, msg: "비밀번호가 틀렸습니다."};
+        }
        
-        // return { success: false, msg: "존재하지 않는 아이디입니다."};
+        return { success: false, msg: "존재하지 않는 아이디입니다."};
 
     }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const fs = require("fs");
+//fs는 프로미스를 반환한다.
+const fs = require("fs").promises;
 
 class UserStorage {
     
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   // => [id, psword, name]
+        //Object.keys??
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    }
 
     //데이터의 은닉화, 메서드로 전달.
     //...변수 로 받은 인자가 배열로 전달된다.
@@ -21,21 +33,15 @@ class UserStorage {
     static getUserInfo(id) {
         // const users = this.#users;
         //현재경로는 app.js의 경로임
-        return fs.readFile("./src/databases/users.json", (err, data) => {
-            if (err) throw err;
-            const users = JSON.parse(data);
-            const idx = users.id.indexOf(id);
-            const usersKeys = Object.keys(users);   // => [id, psword, name]
-            //Object.keys??
-            const userInfo = usersKeys.reduce((newUser, info) => {
-                newUser[info] = users[info][idx];
-                return newUser;
-            }, {});
-
-            return userInfo;
-        });
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+          .catch(console.error);
     }
 
+    
     static save(userInfo) {
         // const users = this.#users;
         users.id.push(userInfo.id);
